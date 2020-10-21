cab7d8b8b3aedf2d1c7a28ad27d263f3784e8f94
Matthew Everett <matthew@mjheverett.com>
2020-10-21 15:26:46 -0400

    Fixed issue in refactor

--- src/redux/reducers/name.js
+++ src/redux/reducers/name.js
@@ -1,17 +1,12 @@
 import { SET_NAME } from '../actionTypes';
 
-const initialState = {
-    name: "Guster",
-}
+const initialState = "Guster";
 
 const nameReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_NAME: {
             const { name } = action.payload;
-            return {
-                ...state,
-                name: name
-            }
+            return name;
         }
         default:
             return state;
